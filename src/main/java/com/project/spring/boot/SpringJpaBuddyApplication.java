9d3b5a789acce8c1615803195c405bcf6c9ebec6
Kumar Gaurav <kgaurav4291@gmail.com>
2025-09-03 16:50:45 +0530

    init commit

---
+++ src/main/java/com/project/spring/boot/SpringJpaBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/spring/boot/SpringJpaBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
